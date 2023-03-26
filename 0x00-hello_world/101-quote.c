f0aa13d1be44f6cd9b285ee1d5eee3d20e9f9aec
Adora24 <oziomaadora@gmail.com>
2023-03-26 10:33:01 -0700

    C Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 #include <unistd.h>
 
@@ -9,8 +8,7 @@
  */
 int main(void)
 {
-        writer(2, 
+	writer(2,
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-        return (1);
+	return (1);
 }
