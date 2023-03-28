12fffbc2a2f9e787cb95d8785d11f5becc9dc63f
Adora24 <oziomaadora@gmail.com>
2023-03-28 16:51:43 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - to print string revserve
+ * @s: function parameters
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		count++;
+	for (i = count ; i >= 0 ; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
