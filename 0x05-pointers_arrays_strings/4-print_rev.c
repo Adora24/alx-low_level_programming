f611f534f86c6c47e2303ea0ec6c2b0780278974
Adora24 <oziomaadora@gmail.com>
2023-03-28 17:54:20 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,18 +2,18 @@
 
 /**
  * print_rev - to print string revserve
- * @str: function parameters
+ * @s: function parameters
  * Return: 0
  */
 
-void print_rev(char *str)
+void print_rev(char *s)
 {
 	int i;
 	int count = 0;
 
-	for (i = 0 ; str[i] != '\0' ; i++)
+	for (i = 0 ; s[i] != '\0' ; i++)
 		count++;
 	for (i = count ; i >= 0 ; i--)
-		_putchar(str[i]);
+		_putchar(s[i]);
 	_putchar('\n');
 }
